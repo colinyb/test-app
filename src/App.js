d69f087de765243a163b2ce6324019b5fc0b6e79
colinyb <cry4@uw.edu>
2019-02-13 17:18:27 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,49 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import PubNubReact from 'pubnub-react';
+import Button from '@material-ui/core/Button';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.pubnub = new PubNubReact({
+      publishKey: 'pub-c-4d1fc396-e440-4867-b919-f1b511a299c9',
+      subscribeKey: 'sub-c-66c71010-2ff1-11e9-9089-0a173ec63317'
+    });
+    this.pubnub.init(this);
+
+    this.state = {
+      headerText: "Button Pressed",
+      clickCount: 0
+    }
+  }
+// componentWillMount() {
+//   this.pubnub.subscribe({
+//     channels:['Default'],
+//     withPresence: true
+//   });
+
+//   this.pubnub.getMessage('Default', (message) => {
+//     console(message);
+//   });
+
+//   this.pubnub.getStatus((st) => {
+//     this.pubnub.publish({
+//         message: 'hello world from react',
+//         channel: 'channel1'
+//     });
+//   });
+// }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            {this.state.headerText + ": " + this.state.clickCount}
           </p>
           <a
             className="App-link"
@@ -19,6 +53,11 @@ class App extends Component {
           >
             Learn React
           </a>
+          <Button onClick={() => {
+            this.setState({
+              clickCount: this.state.clickCount + 1
+            });
+          }} variant="contained" color="primary">Press Me</Button>
         </header>
       </div>
     );
