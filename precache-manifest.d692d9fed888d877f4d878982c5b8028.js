526369c693d8bbd3eaec0db53b635bf0cbba3044
colinyb <cry4@uw.edu>
2019-02-13 17:26:31 -0800

    Updates

---
+++ precache-manifest.d692d9fed888d877f4d878982c5b8028.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cfbed543e292f9d26725",
+    "url": "/test-app/static/css/main.9d22a246.chunk.css"
+  },
+  {
+    "revision": "cfbed543e292f9d26725",
+    "url": "/test-app/static/js/main.cfbed543.chunk.js"
+  },
+  {
+    "revision": "c3af9d7e88abdfb8c56c",
+    "url": "/test-app/static/js/runtime~main.c3af9d7e.js"
+  },
+  {
+    "revision": "32a8655bdd220858fff2",
+    "url": "/test-app/static/js/2.32a8655b.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f5b25f6d36d6f2356060085c2422850d",
+    "url": "/test-app/index.html"
+  }
+];
